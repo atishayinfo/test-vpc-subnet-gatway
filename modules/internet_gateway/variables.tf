20a0f38f6c6de861d212e6642f2ebdb72abab74c
Atishay <atishay2024@outlook.com>
2024-11-23 22:55:03 +0530

    Update variables.tf

--- modules/internet_gateway/variables.tf
+++ modules/internet_gateway/variables.tf
@@ -1,10 +1,9 @@
 variable "vpc_id" {
-  description = "The ID of the VPC to attach the Internet Gateway to."
+  description = "The ID of the VPC."
   type        = string
 }
 
 variable "igw_name" {
   description = "The name of the Internet Gateway."
   type        = string
-  default     = "my-internet-gateway" # Optional default value
 }
